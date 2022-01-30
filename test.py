87d1e3a336e70a35894173ff25d5dc423972d7e6
semlele <ken99suzu@icloud.com>
2022-01-31 00:09:36 +0900

    changed code to delele warning

--- test.py
+++ test.py
@@ -13,10 +13,9 @@ for i in range(10,len(df_ranking)):
     if (df_ranking["score"][i-1] != df_ranking["score"][i]):
         df_ranking_top = df_ranking.head(i)
         break
-    
-#rankメソッドで順位付け
-df_ranking_top["rank"] = df_ranking_top.rank(ascending=False,numeric_only=True,method='min').astype('int')
-df_ranking_top = df_ranking_top.reindex(columns=['rank', 'player_id', 'score'])
+
+#rankメソッドで順位付けし、先頭列にrankを追加
+df_ranking_top.insert(0, "rank", df_ranking_top["score"].rank(ascending=False, method='min').astype(int))
 
 #3列のCSV形式で出力
 df_ranking_top.to_csv("output.csv", index=False)
